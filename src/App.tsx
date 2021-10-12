1825f9740e7356427c0418419aaefd008c9666e9
Alger <alger.chen23@gmail.com>
2021-10-13 03:23:19 +0800

    Add watch build for development

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [file, setFile] = useState<string>('');
-
-  const openFile = async () => {
-    let response = await Neutralino.os.showDialogOpen({
-      title: 'Select a folder',
-      isDirectoryMode: false,
-      filter: ['json', '*']
-    });
-    console.log(`You've selected: ${response.selectedEntry}`);
-    setFile(response.selectedEntry);
-  };
-
   return (
     <div className="App">
-      <button onClick={openFile}>Open File</button>
-      <br/>
-      {file}
+      React
     </div>
   );
 }
